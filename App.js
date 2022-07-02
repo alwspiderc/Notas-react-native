39fc37a345c2ddf422560724ee4082b64f49a592
Ester <alwesterc@gmail.com>
2022-07-02 16:22:48 -0300

    projeto notas usando sqlite

---
+++ App.js
@@ -0,0 +1,42 @@
+import { SafeAreaView, StatusBar, StyleSheet, FlatList} from "react-native"
+import NotaEditor from "./src/componentes/NotaEditor"
+import { Nota } from "./src/componentes/Nota"
+import { useEffect, useState } from "react"
+import { buscaNotas, criaTabela } from "./src/servicos/Notas"
+
+export default function App() {
+ 
+  useEffect(() => {
+    criaTabela()
+    mostraNotas()
+  }, [])
+ 
+  const [notaSelecionada, setNotaSelecionada] = useState({})
+  const [notas, setNotas] = useState([])
+
+  async function mostraNotas() {
+    const todasNotas = await buscaNotas()
+    setNotas(todasNotas)
+  }
+
+  return (
+    <SafeAreaView style={estilos.container}>
+      <FlatList
+        data={notas}
+        renderItem={(nota) => <Nota {...nota} setNotaSelecionada={setNotaSelecionada}/>}
+        keyExtractor={nota => nota.id}
+      />
+      <NotaEditor mostraNotas={mostraNotas} notaSelecionada={notaSelecionada} setNotaSelecionada={setNotaSelecionada}/>
+      <StatusBar/>
+    </SafeAreaView>
+  )
+}
+
+const estilos = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "stretch",
+		justifyContent: "flex-start",
+	},
+})
+
